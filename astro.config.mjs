be4dd02de537d397dde61daa08c8d8bc78817ce0
Zachary Matus <zachary.matus@gmail.com>
2026-04-19 15:22:51 -0500

    rename repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zmatus.github.io/portfolio',
+  site: 'https://zmatus.github.io',
   base: '/portfolio/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
